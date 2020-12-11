1892f87d958825869ae52b91adb1d0cd161edea8
merima98 <merimaceranic98@gmail.com>
2020-12-11 14:32:23 +0100

    Add changes at the time of posting

--- src/features/posts/components/Post.js
+++ src/features/posts/components/Post.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { formatDistanceToNow, format } from "date-fns";
+import { format } from "date-fns";
 import styled from "styled-components";
 import { BREAKPOINTS } from "../../../constants";
 
@@ -41,28 +41,52 @@ const Time = styled.span`
 
 function Post(props) {
   const { body, date } = props;
-  // const dateToConvert = formatDistanceToNow(Date.parse(date));
-  // const dateToConvert = Date.parse(date);
-  // const monthNames = [
-  //   "January",
-  //   "February",
-  //   "March",
-  //   "April",
-  //   "May",
-  //   "June",
-  //   "July",
-  //   "August",
-  //   "September",
-  //   "October",
-  //   "November",
-  //   "December",
-  // ];
-  // const month = monthNames[dateToConvert.getMonth()];
-  console.log(format(Date.parse(date), "yyyy/MM/dd kk:mm:ss"));
+
+  let time = `${format(Date.parse(date), "dd.MM.yyyy")}. at ${format(
+    Date.parse(date),
+    "kk:mm"
+  )}`;
+
+  if (
+    format(Date.now(), "yyyy/MM/dd") === format(Date.parse(date), "yyyy/MM/dd")
+  ) {
+    if (
+      Number(format(Date.parse(date), "kk")) < Number(format(Date.now(), "kk"))
+    ) {
+      time = `Before ${Number(
+        format(Date.now(), "kk") - Number(format(Date.parse(date), "kk"))
+      )} hours`;
+    }
+    if (
+      Number(format(Date.parse(date), "kk")) ===
+      Number(format(Date.now(), "kk"))
+    ) {
+      if (
+        Number(format(Date.parse(date), "mm")) >
+        Number(format(Date.now(), "mm"))
+      ) {
+        time = `Before ${
+          Number(format(Date.now(), "mm")) +
+          (60 - Number(format(Date.parse(date), "mm")))
+        } minuts`;
+      } else if (
+        Number(format(Date.now(), "mm")) -
+          Number(format(Date.parse(date), "mm")) <
+        1
+      ) {
+        time = `Just now`;
+      } else {
+        time = `Before ${
+          Number(format(Date.now(), "mm")) -
+          Number(format(Date.parse(date), "mm"))
+        } minuts`;
+      }
+    }
+  }
   return (
     <StyledPost>
       <Header>
-        <Time>{formatDistanceToNow(Date.parse(date))} ago</Time>
+        <Time>{time}</Time>
       </Header>
       <Main>
         <Body>{body}</Body>
