7665ee09a97e21e9866e3a452d344ca90c79d111
merima98 <merimaceranic98@gmail.com>
2021-01-07 15:58:28 +0100

    Adding changes to header

--- src/features/header/components/Header.js
+++ src/features/header/components/Header.js
@@ -1,46 +1,43 @@
 import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-
 import {
-  MdAutorenew,
-  MdFilterTiltShift,
-  MdHome,
-  MdKeyboardArrowDown,
-  MdKeyboardArrowUp,
-  MdWbIncandescent,
-} from "react-icons/md";
+  Home,
+  Sun,
+  Moon,
+  ArrowUp,
+  ArrowDown,
+  Filter,
+  RefreshCcw,
+} from "react-feather";
 
 import { BREAKPOINTS } from "../../../constants";
 
 const StyledHeader = styled.header`
-  padding: 1rem 0.5rem;
   border-bottom: 1px solid #808080;
-
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   background-color: ${(props) => props.theme.colors.background};
-
-  @media (min-width: ${BREAKPOINTS.SMALL_DEVICES}) {
-    width: 100%;
-  }
+  height: 3.5rem;
 `;
 
 const Container = styled.div`
-  padding: 0 20px;
+  width: 50%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 0.5rem;
   @media (min-width: ${BREAKPOINTS.SMALL_DEVICES}) {
-    max-width: 500px;
-    margin: 0 auto;
+    grid-gap: 1rem;
   }
 `;
 
-const Home = styled(NavLink)`
-  margin-right: 0.5rem;
+const Links = styled(NavLink)`
+  padding: 16px;
   color: black;
   text-decoration: none;
-  align-items: center;
 
   &.active {
     color: black;
@@ -51,8 +48,27 @@ const Home = styled(NavLink)`
   }
 
   @media (min-width: ${BREAKPOINTS.SMALL_DEVICES}) {
-    height: 20px;
+    left: 0;
+    &.active {
+      padding-bottom: 0px;
+    }
+  }
+`;
+
+const DatkTheme = styled.div`
+  padding: 16px;
+  color: black;
+  text-decoration: none;
+
+  &.active {
+    color: black;
+    text-decoration: none;
     width: 20px;
+    padding-bottom: 8px;
+    border-bottom: 2px solid #1877f2;
+  }
+
+  @media (min-width: ${BREAKPOINTS.SMALL_DEVICES}) {
     left: 0;
     &.active {
       padding-bottom: 0px;
@@ -64,30 +80,36 @@ function Header(props) {
   return (
     <StyledHeader>
       <Container>
-        <Home exact to="/">
-          <MdHome to="/" style={{ color: `#1877F2` }} />
-        </Home>
-        <MdWbIncandescent
-          onClick={() => {
-            props.setIsDarkMode(!props.isDarkMode);
-          }}
-          style={{ color: `#65676b` }}
-        ></MdWbIncandescent>
-        <Home exact to="/sort/lastadded">
-          <MdAutorenew to="/sort/lastadded" style={{ color: `#65676b` }} />
-        </Home>
-        <Home exact to="/sort/upvoted">
-          <MdKeyboardArrowUp to="/sort/upvoted" style={{ color: `#65676b` }} />
-        </Home>{" "}
-        <Home exact to="/sort/downvoted">
-          <MdKeyboardArrowDown
-            to="/sort/downvoted"
-            style={{ color: `#65676b` }}
-          />
-        </Home>
-        <Home exact to="/sort/random">
-          <MdFilterTiltShift to="/sort/random" style={{ color: `#65676b` }} />
-        </Home>
+        <Links exact to="/">
+          <Home to="/" style={{ color: `#1877F2` }} />
+        </Links>
+        <DatkTheme>
+          {props.isDarkMode ? (
+            <Moon
+              onClick={() => {
+                props.setIsDarkMode(!props.isDarkMode);
+              }}
+            />
+          ) : (
+            <Sun
+              onClick={() => {
+                props.setIsDarkMode(!props.isDarkMode);
+              }}
+            />
+          )}
+        </DatkTheme>
+        <Links exact to="/sort/lastadded">
+          <RefreshCcw to="/sort/lastadded" />
+        </Links>
+        <Links exact to="/sort/upvoted">
+          <ArrowUp to="/sort/upvoted" />
+        </Links>{" "}
+        <Links exact to="/sort/downvoted">
+          <ArrowDown to="/sort/downvoted" />
+        </Links>
+        <Links exact to="/sort/random">
+          <Filter to="/sort/random" />
+        </Links>
       </Container>
     </StyledHeader>
   );
